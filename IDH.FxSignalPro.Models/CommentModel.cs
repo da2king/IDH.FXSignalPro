9c8e9aaaea2b5ff4c2ca1c7c9de42fef67a4a8bd
Dumasoka <Dumasoka@gmail.com>
2017-03-30 14:46:35 +0200

    Testing check in

--- IDH.FxSignalPro.Models/CommentModel.cs
+++ IDH.FxSignalPro.Models/CommentModel.cs
@@ -5,37 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 namespace IDH.FxSignalPro.Models
 {
-    public  class CommentModel
+    public class CommentModel
     {
         public CommentModel()
         {
-            
-              
+
+
         }
-         
-				public Guid CommentId {get; set;}
+
+        public Guid CommentId { get; set; }
 
 
-				public Guid UserId {get; set;}
+        public Guid UserId { get; set; }
 
 
-				public Guid SignalId {get; set;}
+        public Guid SignalId { get; set; }
 
 
-				public String Message {get; set;}
+        public String Message { get; set; }
 
 
-				public DateTime Created {get; set;}
+        public DateTime Created { get; set; }
 
 
-				public DateTime Deleted {get; set;}
+        public DateTime Deleted { get; set; }
 
 
-				public Boolean IsPublished {get; set;}
+        public Boolean IsPublished { get; set; }
 
 
-				public Boolean IsPreSaleComment {get; set;}
+        public Boolean IsPreSaleComment { get; set; }
 
-    
+
     }
 }
